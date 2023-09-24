eaed29a933304ab54b671b633e8bb3784bdd13ef
twwct <twwc.tech@gmail.com>
2023-09-23 20:14:25 -0600

    static methods completed but not yet tested

--- Backend/SecureGenerator/PassPin.cs
+++ Backend/SecureGenerator/PassPin.cs
@@ -1,15 +1,66 @@
-﻿namespace SimpleSecUtility.Backend.SecureGenerator
+﻿using System.Drawing.Text;
+
+namespace SimpleSecUtility.Backend.SecureGenerator
 {
     internal class PassPin
     {
-        public static string ReturnSecurePassword()
+        private const int _defaultPasswordLength = 8;
+        private const int _defaultPINLength = 4;
+
+        public static async Task<string> ReturnSecurePassword(int passwordLength = _defaultPasswordLength)
         {
-            // Code goes here
+            using (HttpClient passwordClient = new HttpClient())
+            {
+                string passwordResult = string.Empty;
+
+                if (passwordLength >= _defaultPasswordLength)
+                {
+                    string passwordApiUrl = $"https://www.random.org/strings/?num=1&len={passwordLength}&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new";
+                    try
+                    {
+                        passwordClient.DefaultRequestHeaders.Add("X-RandomOrg-ApiKey", ""); // Complete the API Key
+                        HttpResponseMessage response = await passwordClient.GetAsync(passwordApiUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            passwordResult = await response.Content.ReadAsStringAsync();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Unable to connect. Status Code: {response.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex) { ex.ToString(); }
+                }
+                return passwordResult.Trim();
+            }
         }
 
-        public static string ReturnSecurePIN()
+        public static async Task<string> ReturnSecurePIN(int pinLength = _defaultPINLength)
         {
-            // Code goes here
+            using (HttpClient pinClient = new HttpClient())
+            {
+                string pinResult = string.Empty;
+
+                if (pinLength >= _defaultPINLength)
+                {
+                    string pinApiUrl = $"https://www.random.org/strings/?num=1&len={pinLength}&digits=on&format=plain&rnd=new";
+                    try
+                    {
+                        pinClient.DefaultRequestHeaders.Add("X-RandomOrg-ApiKey", ""); // Complete the API Key string
+                        HttpResponseMessage pinResponse = await pinClient.GetAsync(pinApiUrl);
+                        if (pinResponse.IsSuccessStatusCode)
+                        {
+                            pinResult = await pinResponse.Content.ReadAsStringAsync();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Unable to connect. Status Code: {pinResponse.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex) { ex.ToString(); }
+                }
+                return pinResult.Trim();
+            }
         }
     }
 }
