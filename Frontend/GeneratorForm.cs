ca3248823e262533d4d4f3f2a4b8f3c062c693eb
twwct <twwc.tech@gmail.com>
2023-09-24 17:07:39 -0600

    MessageBoxes updated

--- Frontend/GeneratorForm.cs
+++ Frontend/GeneratorForm.cs
@@ -24,12 +24,18 @@
 
         private void PasswordLengthPicker_ValueChanged(object sender, EventArgs e)
         {
-            // Code goes here
+            if (PasswordLengthPicker.Value < 8)
+            {
+                MessageBox.Show("Password must be a minumum length of 8", "Password Length Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void PinLengthPicker_ValueChanged(object sender, EventArgs e)
         {
-            // Code goes here
+            if (PinLengthPicker.Value < 4 ||  PinLengthPicker.Value > 6)
+            {
+                MessageBox.Show("PIN must be a minimum length of 4 and no greater than 6", "PIN Length Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void RequestButton_Click(object sender, EventArgs e)
