f5ae6795a013f8d4b18e3b03aabe3418304a548b
twwct <twwc.tech@gmail.com>
2023-09-24 01:13:04 -0600

    PasswordRequirementsInfo variable created

--- AppResources/Statics.cs
+++ AppResources/Statics.cs
@@ -5,6 +5,8 @@
         public const string AppVersion = "1.0";
         public const string RegPath = @"Software\SimpleSecUtility";
 
+        public const string PasswordRequirementsInfo = "Password Requirements:\n- Mininum of 8 characters or more in length\n- Minimum of 1 Number\n- Minimum of 1 Special Character [-!@#$%^&*()?_,.]";
+
         public static string SetupInfo = "Welcome to the Simple Sec Utility Setup.\n" +
             "The application is setup per user account, each new user\n" +
             "or an existing user, using the app for the first time,\n" +
