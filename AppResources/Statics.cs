59e7244bc7168d53d94bb4741b5809ca5bceee78
twwct <twwc.tech@gmail.com>
2023-09-23 22:02:13 -0600

    file created and constants created

---
+++ AppResources/Statics.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleSecUtility.AppResources
+{
+    internal class Statics
+    {        
+        public const string AppVersion = "1.0";
+        public const string RegPath = @"Software\SimpleSecUtility";
+    }
+}
