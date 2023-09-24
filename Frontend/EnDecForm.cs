b0561891ca9a7f2f0bc9bc561604b9d92d68e1a2
twwct <twwc.tech@gmail.com>
2023-09-24 14:57:40 -0600

    UI created and backend connected

---
+++ Frontend/EnDecForm.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleSecUtility.Frontend
+{
+    public partial class EnDecForm : Form
+    {
+        public EnDecForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EnDecForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Frontend/GeneratorForm.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleSecUtility.Frontend
+{
+    public partial class GeneratorForm : Form
+    {
+        public GeneratorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
