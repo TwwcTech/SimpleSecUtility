45679ab5286866e0e38ea42e48416a32d4d29058
twwct <twwc.tech@gmail.com>
2023-09-23 23:30:00 -0600

    statics variables created

--- AppResources/Statics.cs
+++ AppResources/Statics.cs
@@ -4,5 +4,13 @@
     {        
         public const string AppVersion = "1.0";
         public const string RegPath = @"Software\SimpleSecUtility";
+
+        public static string SetupInfo = "Welcome to the Simple Sec Utility Setup.\n" +
+            "The application is setup per user account, each new user\n" +
+            "or an existing user, using the app for the first time,\n" +
+            " will be asked to begin setup.\n\n" +
+            "To continue with the app, a 'Master Password' must be created\n" +
+            "to ensure only the active user account\n" +
+            $"({Environment.UserName}) has access to the 'File Encryption' tool.";
     }
 }
