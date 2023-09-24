ec1fac21e301046a55e22464057e18edd313e200
twwct <twwc.tech@gmail.com>
2023-09-23 22:20:13 -0600

    class created, not yet tested

--- Backend/Hasher.cs
+++ Backend/Hasher.cs
@@ -1,6 +1,16 @@
-﻿namespace SimpleSecUtility.Backend
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleSecUtility.Backend
 {
     internal class Hasher
     {
+        public static string Hash(string stringToHash)
+        {
+            byte[] stringToHashBytes = Encoding.UTF8.GetBytes(stringToHash);
+            byte[] hashedString = HashAlgorithm.Create("SHA256").ComputeHash(stringToHashBytes);
+
+            return Convert.ToBase64String(hashedString);
+        }
     }
 }
