5f42f0feb1c7c840a7e16b58207aa0bd25163e31
twwct <twwc.tech@gmail.com>
2023-09-23 22:03:06 -0600

    SetupCheck in place

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using SimpleSecUtility.Frontend;
+using SimpleSecUtility.Backend.AppSetup;
+
 namespace SimpleSecUtility
 {
     public partial class MainForm : Form
@@ -9,6 +12,22 @@ namespace SimpleSecUtility
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            bool setupStatus = SetupManager.HasAppBeenSetup();
+            if (setupStatus is false)
+            {
+                DialogResult continueSetupResult = MessageBox.Show("Simple Sec Utility has never been run. Would you like to continue with seutp?", "Simple Sec Utility Setup", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                if (continueSetupResult == DialogResult.Yes)
+                {
+                    Form loginForm = new TestLoginForm();
+                    loginForm.ShowDialog();
+                }
+                else if (continueSetupResult == DialogResult.No)
+                {
+                    Close();
+                    Dispose();
+                }
+            }
+
             toolTip = new ToolTip();
             toolTip.SetToolTip(PassPinButton, "Generate a Secure Password or PIN");
             toolTip.SetToolTip(FileEnDecButton, "Encrypt or Decrypt a File");
