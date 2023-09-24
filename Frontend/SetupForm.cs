8d434daf11d7204d2008e13a1cc0611455a5fa34
twwct <twwc.tech@gmail.com>
2023-09-23 22:20:43 -0600

    SetupForm window added

---
+++ Frontend/SetupForm.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleSecUtility.Frontend
+{
+    public partial class SetupForm : Form
+    {
+        public SetupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetupForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
