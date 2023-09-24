0a1ccefc7a32639328ec6e6a3a217e1e0d672378
twwct <twwc.tech@gmail.com>
2023-09-23 22:02:26 -0600

    changed method to statics

---
+++ Backend/AppSetup/SetupManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using SimpleSecUtility.AppResources;
+
+namespace SimpleSecUtility.Backend.AppSetup
+{
+    internal class SetupManager
+    {
+        public static bool HasAppBeenSetup()
+        {
+            using (RegistryKey setupStatusKey = Registry.CurrentUser.OpenSubKey(Statics.RegPath)!)
+            {
+                try
+                {
+                    if (setupStatusKey != null)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception ex) { ex.ToString(); }
+                return false;
+            }
+        }
+
+        public static void RunSetup(string masterPassword)
+        {
+            using (RegistryKey setupKey = Registry.CurrentUser.CreateSubKey(Statics.RegPath))
+            {
+                if (setupKey != null)
+                {
+                    setupKey.SetValue("Setup Date", DateTime.Now.ToString("ddMMyyyy"));
+                    setupKey.SetValue("Version", ""); // Complete the string
+
+                }
+            }
+        }
+    }
+}
