076b581e67b362219597ef0ac3c29e8f8d311635
twwct <twwc.tech@gmail.com>
2023-09-24 13:12:49 -0600

    class created, not yet tested

--- Backend/AppRegistry/RegistryReader.cs
+++ Backend/AppRegistry/RegistryReader.cs
@@ -1,7 +1,30 @@
-﻿namespace SimpleSecUtility.Backend.AppRegistry
+﻿using Microsoft.Win32;
+using SimpleSecUtility.AppResources;
+
+namespace SimpleSecUtility.Backend.AppRegistry
 {
     internal class RegistryReader
     {
-        // Code goes here
+        public static string ReadApiKey(string keyName)
+        {
+            using (RegistryKey readKey = Registry.CurrentUser.OpenSubKey(Statics.RegPath)!)
+            {
+                if (readKey != null)
+                {
+                    switch (keyName.ToLower())
+                    {
+                        case "setupdate":
+                            return readKey.GetValue("Setup Date")!.ToString()!;
+                        case "version":
+                            return readKey.GetValue("Version")!.ToString()!;
+                        case "apikey":
+                            return readKey.GetValue("API Key")!.ToString()!;
+                        default:
+                            throw new ArgumentException("Not an option. Please choose from: [setupdate, version, apikey]");
+                    }
+                }
+                return null!;
+            }
+        }
     }
 }
