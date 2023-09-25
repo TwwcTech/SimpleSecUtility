8a2bce21000d499032966e5a47e36701232b09f0
twwct <twwc.tech@gmail.com>
2023-09-24 22:32:46 -0600

    class created, not yet tested

--- Backend/SecureGenerator/JsonResponseParser.cs
+++ Backend/SecureGenerator/JsonResponseParser.cs
@@ -2,5 +2,24 @@
 {
     internal class JsonResponseParser
     {
+        string? jsonrpc { get; set; }
+        Result? result { get; set; }
+        int id { get; set; }
+    }
+
+    public class Result
+    {
+        Random? random { get; set; }
+        int bitsUsed { get; set; }
+        long bitsLeft { get; set; }
+        long requestsLeft { get; set; }
+        int advisoryDelay { get; set; }
+
+    }
+
+    public class Random
+    {
+        string? data { get; set; }
+        string? completionTime { get; set; }
     }
 }
