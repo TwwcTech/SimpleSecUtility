61c160fe40a3ae424e26269b07bf6a57307e2745
twwct <twwc.tech@gmail.com>
2023-09-24 22:12:50 -0600

    new requestbody string updated for pin method

--- Backend/SecureGenerator/PassPin.cs
+++ Backend/SecureGenerator/PassPin.cs
@@ -30,7 +30,8 @@ namespace SimpleSecUtility.Backend.SecureGenerator
                     case "pin":
                         // add the if branch for pin length: if (pinLength == 4) else if (pinLength > 4)
                         apiUrl = $"https://www.random.org/strings/?num=1&len={requestLength}&digits=on&unique=on&format=plain&rnd=new";
-                        //string requestPinBody = $"{{\"jsonrpc\": \"2.0\", \"method\": \"generateStrings\", \"params\": {{ \"apiKey\": \"{}\", \"n\": 1, \"length\": {requestLength}, \"characters\": \"abcdefghijklmnopqrstuvwxyz1234567890!@#$%&\", \"replacement\": true}}, \"id\": 20251}}";
+                        //string requestPinBody = $"{{\"jsonrpc\": \"2.0\", \"method\": \"generateStrings\", \"params\": {{ \"apiKey\": \"{}\", \"n\": 1, \"length\": {requestLength}, \"characters\": \"1234567890\", \"replacement\": true}}, \"id\": 20251}}";
+                        //string requestPinBody = $"{{\"jsonrpc\": \"2.0\", \"method\": \"generateStrings\", \"params\": {{ \"apiKey\": \"{}\", \"n\": 1, \"length\": {requestLength}, \"characters\": \"1234567890!@#$%&+-()_=\", \"replacement\": true}}, \"id\": 20251}}";
                         break;
                     default:
                         throw new ArgumentException("Not an option, please select from: [password, pin]");
