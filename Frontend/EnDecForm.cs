0e560de14e5531dff26815f67bc345396920aecc
twwct <twwc.tech@gmail.com>
2023-09-26 00:51:33 -0600

    backend updated

--- Frontend/EnDecForm.cs
+++ Frontend/EnDecForm.cs
@@ -2,6 +2,9 @@
 {
     public partial class EnDecForm : Form
     {
+        string? _fileToEncrypt;
+        string? _fileToDecrypt;
+
         public EnDecForm()
         {
             InitializeComponent();
@@ -9,7 +12,14 @@
 
         private void EnDecForm_Load(object sender, EventArgs e)
         {
-
+            //    fileOpener = new OpenFileDialog();
+            //    fileOpener.Title = "Select a file to ecnrypt";
+            //    fileOpener.Filter = "All files | *.*";
+            //    fileOpener.InitialDirectory = @"C:\";
+            //    if (fileOpener.ShowDialog() == DialogResult.OK)
+            //    {
+            //        _fileToEncrypt = fileOpener.FileName;
+            //    }
         }
     }
 }
