c8cb2bc13a53cd373fc4bfedaf402083321beb93
twwct <twwc.tech@gmail.com>
2023-09-27 20:28:48 -0600

    encrypted method completed not implemented nor tested

--- Backend/EnDec/FileEnDec.cs
+++ Backend/EnDec/FileEnDec.cs
@@ -7,27 +7,58 @@ namespace SimpleSecUtility.Backend.EnDec
     {
         private static readonly byte[] _IV = Encoding.UTF8.GetBytes("1234567890");
 
-        public static void EncryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string fileToEncrypt, string password)
+        public static void EncryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string password)
         {
-            //using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
-            //{
-            //    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-            //    using (ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV))
-            //    {
-            //        // Code to save file may possibly go here
+            openFileDialog.Title = "Select a file to ecnrypt";
+            openFileDialog.Filter = "All files | *.*";
+            openFileDialog.InitialDirectory = @"C:\";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                saveFileDialog.Title = "Select a Destination for Encrypted File";
+                saveFileDialog.Filter = "Encrypted files | (*.enc)|*.enc*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (FileStream inputSream = new(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (FileStream outputStream = new(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
+                        {
+                            using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
+                            {
+                                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                                using (ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV))
+                                {
+                                    using (CryptoStream cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write))
+                                    {
+                                        byte[] buffer = new byte[1024];
+                                        int byteRead;
 
-            //        // else, "using (FileStream outputStream) { }"
-            //        using (CryptoStream cryptoStream = new CryptoStream())
-            //        {
-
-            //        }
-            //    }
-            //}
+                                        while ((byteRead = inputSream.Read(buffer, 0, buffer.Length)) > 0)
+                                        {
+                                            cryptoStream.Write(buffer, 0, byteRead);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        public static void DecryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string fileToDecrypt)
+        public static void DecryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string password)
         {
-            // Code goes here
+            openFileDialog.Title = "Select a File to Decrypt";
+            openFileDialog.Filter = "Encrypted files | (*.enc)|*.enc*";
+            openFileDialog.InitialDirectory = @"C:\";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                saveFileDialog.Title = "Select a Destination for Decrypted File";
+                saveFileDialog.Filter = "All files | *.*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Decrypt method goes here
+                }
+            }
         }
     }
 }
