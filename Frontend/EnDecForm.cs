03969eb6f8037d1f802967e0f01fdd99349e1c66
twwct <twwc.tech@gmail.com>
2023-09-27 21:26:09 -0600

    Testing removed

--- Frontend/EnDecForm.cs
+++ Frontend/EnDecForm.cs
@@ -20,45 +20,46 @@ namespace SimpleSecUtility.Frontend
 
         private void encryptButton_Click(object sender, EventArgs e)
         {
-            FileEnDec.EncryptFile(fileOpener, fileSaver, _fileToEncrypt!, "password1234");
-            fileOpener = new OpenFileDialog();
-            fileOpener.Title = "Select a file to ecnrypt";
-            fileOpener.Filter = "All files | *.*";
-            fileOpener.InitialDirectory = @"C:\";
-            if (fileOpener.ShowDialog() == DialogResult.OK)
-            {
-                _fileToEncrypt = fileOpener.FileName;
-            }
+            FileEnDec.EncryptFile(fileOpener, fileSaver, "");
+            //fileOpener = new OpenFileDialog();
+            //fileOpener.Title = "Select a file to ecnrypt";
+            //fileOpener.Filter = "All files | *.*";
+            //fileOpener.InitialDirectory = @"C:\";
+            //if (fileOpener.ShowDialog() == DialogResult.OK)
+            //{
+            //    _fileToEncrypt = fileOpener.FileName;
+            //}
 
-            if (!string.IsNullOrEmpty(_fileToEncrypt) )
-            {
-                //FileEnDec.EncryptFile(_fileToEncrypt);
-            }
-            else
-            {
-                MessageBox.Show("No file to encrypt", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
+            //if (!string.IsNullOrEmpty(_fileToEncrypt) )
+            //{
+            //    //FileEnDec.EncryptFile(_fileToEncrypt);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("No file to encrypt", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //}
         }
 
         private void decryptButton_Click(object sender, EventArgs e)
         {
-            fileOpener = new OpenFileDialog();
-            fileOpener.Title = "Select a file to decrypt";
-            fileOpener.Filter = "All files | *.*";
-            fileOpener.InitialDirectory = @"C:\";
-            if (fileOpener.ShowDialog() == DialogResult.OK)
-            {
-                _fileToDecrypt = fileOpener.FileName;
-            }
+            FileEnDec.DecryptFile(fileOpener, fileSaver, "");
+            //fileOpener = new OpenFileDialog();
+            //fileOpener.Title = "Select a file to decrypt";
+            //fileOpener.Filter = "All files | *.*";
+            //fileOpener.InitialDirectory = @"C:\";
+            //if (fileOpener.ShowDialog() == DialogResult.OK)
+            //{
+            //    _fileToDecrypt = fileOpener.FileName;
+            //}
 
-            if (!string.IsNullOrEmpty(_fileToDecrypt))
-            {
-                //FileEnDec.DecryptFile(_fileToDecrypt);
-            }
-            else
-            {
-                MessageBox.Show("No file to decrypt", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
+            //if (!string.IsNullOrEmpty(_fileToDecrypt))
+            //{
+            //    //FileEnDec.DecryptFile(_fileToDecrypt);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("No file to decrypt", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //}
         }
     }
 }
