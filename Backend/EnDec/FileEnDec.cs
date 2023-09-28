cd0bac2eabdad5cb4dc2a33a443bb960ce97644d
twwct <twwc.tech@gmail.com>
2023-09-27 18:32:27 -0600

    method updated and comments created

--- Backend/EnDec/FileEnDec.cs
+++ Backend/EnDec/FileEnDec.cs
@@ -7,21 +7,25 @@ namespace SimpleSecUtility.Backend.EnDec
     {
         private static readonly byte[] _IV = Encoding.UTF8.GetBytes("1234567890");
 
-        public static void EncryptFile(string fileToEncrypt, string password)
+        public static void EncryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string fileToEncrypt, string password)
         {
-            using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
-            {
-                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-                using (ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV))
-                {
-                    // Code to save file may possibly go here
+            //using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
+            //{
+            //    byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            //    using (ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV))
+            //    {
+            //        // Code to save file may possibly go here
 
-                    // else, "using (FileStream outputStream) { }"
-                }
-            }
+            //        // else, "using (FileStream outputStream) { }"
+            //        using (CryptoStream cryptoStream = new CryptoStream())
+            //        {
+
+            //        }
+            //    }
+            //}
         }
 
-        public static void DecryptFile(string fileToDecrypt)
+        public static void DecryptFile(OpenFileDialog openFileDialog, SaveFileDialog saveFileDialog, string fileToDecrypt)
         {
             // Code goes here
         }
