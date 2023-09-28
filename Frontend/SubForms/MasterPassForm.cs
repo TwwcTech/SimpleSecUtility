05caec537c1581ff2ba1892e3fc528557486cf1a
twwct <twwc.tech@gmail.com>
2023-09-27 22:27:20 -0600

    button backend connected and logic created

--- Frontend/SubForms/MasterPassForm.cs
+++ Frontend/SubForms/MasterPassForm.cs
@@ -1,4 +1,8 @@
-﻿namespace SimpleSecUtility.Frontend.SubForms
+﻿using SimpleSecUtility.Backend;
+using SimpleSecUtility.Backend.AppRegistry;
+using SimpleSecUtility.Backend.InputChecks;
+
+namespace SimpleSecUtility.Frontend.SubForms
 {
     public partial class MasterPassForm : Form
     {
@@ -10,6 +14,28 @@
         private void MasterPassForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void submitMasterPassButton_Click(object sender, EventArgs e)
+        {
+            TextBox[] masterPasswordTextbox = new TextBox[] { masterPassTextbox };
+            bool isMasterPasswordTextboxEmpty = EmptyChecks.Instance.AreTextboxInputsEmpty(masterPasswordTextbox);
+
+            if (!isMasterPasswordTextboxEmpty )
+            {
+                if (PasswordSecurityCheck.Instance!.IsPasswordSecure(masterPassTextbox.Text))
+                {
+                    string hashedMasterPass = Hasher.Hash(masterPassTextbox.Text);
+                    if (hashedMasterPass == RegistryReader.ReadApiKey("master"))
+                    {
+                        Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect Master Password", "Master Password Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
