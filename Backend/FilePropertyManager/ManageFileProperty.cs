85dcb0399233973874e6d55e5dc8d20ee218228c
twwct <twwc.tech@gmail.com>
2023-10-01 15:34:55 -0600

    method udpates and testing added

--- Backend/FilePropertyManager/ManageFileProperty.cs
+++ Backend/FilePropertyManager/ManageFileProperty.cs
@@ -1,10 +1,18 @@
-﻿namespace SimpleSecUtility.Backend.FilePropertyManager
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleSecUtility.Backend.FilePropertyManager
 {
     internal class ManageFileProperty
     {
         public static void AddGuidToFileProperty(string filePath, string guid)
         {
-            // Code goes here
+
+        }
+
+        public static bool IsGuid(string guid)
+        {
+            Regex guidRegex = new Regex(@"^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$");
+            return guidRegex.IsMatch(guid);
         }
 
         public static string GetGuidFromFile(string filePath)
