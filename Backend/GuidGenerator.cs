af5b2d9c52703eb202a2943a307521fe6a1390e9
twwct <twwc.tech@gmail.com>
2023-10-01 15:37:13 -0600

    method to check if string is guid created

--- Backend/GuidGenerator.cs
+++ Backend/GuidGenerator.cs
@@ -1,10 +1,18 @@
-﻿namespace SimpleSecUtility.Backend
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleSecUtility.Backend
 {
     internal class GuidGenerator
     {
         public static string GenerateNewGuid()
         {
             return Guid.NewGuid().ToString();
+        }
+
+        public static bool IsGuid(string guid)
+        {
+            Regex guidRegex = new Regex(@"^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$");
+            return guidRegex.IsMatch(guid);
         }
     }
 }
