2d252d27ce43d9ae478f3e4e38424203912718c2
twwct <twwc.tech@gmail.com>
2023-10-08 15:24:54 -0600

    class created

--- Backend/AppRegistry/RegistryWriter.cs
+++ Backend/AppRegistry/RegistryWriter.cs
@@ -1,6 +1,19 @@
-﻿namespace SimpleSecUtility.Backend.AppRegistry
+﻿using SimpleSecUtility.AppResources;
+using Microsoft.Win32;
+
+namespace SimpleSecUtility.Backend.AppRegistry
 {
     internal class RegistryWriter
     {
+        public static void WriteFileNameHashAndGUID(string fileNameHash, string guid)
+        {
+            using (RegistryKey writeFileNameHashKey = Registry.CurrentUser.OpenSubKey(Statics.RegPath + @"\EnDecFileExtensions")!)
+            {
+                if (writeFileNameHashKey != null)
+                {
+                    writeFileNameHashKey.SetValue(fileNameHash, guid);
+                }
+            }
+        }
     }
 }
