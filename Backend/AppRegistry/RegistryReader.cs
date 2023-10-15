d0a484eb5e5151fa542a54c4b7c620bad7e39687
twwct <twwc.tech@gmail.com>
2023-10-15 13:21:13 -0600

    static method and step comment removed

--- Backend/AppRegistry/RegistryReader.cs
+++ Backend/AppRegistry/RegistryReader.cs
@@ -3,40 +3,8 @@ using SimpleSecUtility.AppResources;
 
 namespace SimpleSecUtility.Backend.AppRegistry
 {
-    // STEP 4 - Create method to call API Key from the Windows Credential Manager before removing method
-
     internal class RegistryReader
     {
-        public static string ReadApiKey(string keyName)
-        {
-            /* Remove this method or update the method name
-             * because all API calls will be from the 
-             * Windows Credential Manager. The method here
-             * should be able to call or Read the file extension
-             * thats in the EnDecFileExtensions Registry SubKey */
-
-            using (RegistryKey readKey = Registry.CurrentUser.OpenSubKey(Statics.RegPath)!)
-            {
-                if (readKey != null)
-                {
-                    switch (keyName.ToLower())
-                    {
-                        case "setupdate":
-                            return readKey.GetValue("Setup Date")!.ToString()!;
-                        case "version":
-                            return readKey.GetValue("Version")!.ToString()!;
-                        case "apikey":
-                            return readKey.GetValue("API Key")!.ToString()!;
-                        case "master":
-                            return readKey.GetValue("Master")!.ToString()!;
-                        default:
-                            throw new ArgumentException("Not an option. Please choose from: [setupdate, version, apikey]");
-                    }
-                }
-                return null!;
-            }
-        }
-
         public static bool DoesHashedPathExist(string keyName)
         {
             using (RegistryKey hashedPathKey = Registry.CurrentUser.OpenSubKey(Statics.RegPath + @"\EnDecFileExtensions")!)
