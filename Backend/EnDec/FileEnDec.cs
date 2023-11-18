4dff2f6db6b16feb05834f290dd97fa861a4eff0
twwct <twwc.tech@gmail.com>
2023-11-18 13:38:48 -0700

    using declarations simplified

--- Backend/EnDec/FileEnDec.cs
+++ Backend/EnDec/FileEnDec.cs
@@ -12,18 +12,22 @@ namespace SimpleSecUtility.Backend.EnDec
 
         public static void ProvisionAndEncryptFile(string password) // Explore the idea of passing through a Form object to open the password promtps, or not and continue with the script!
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Title = "Select a file to ecnrypt";
-            openFileDialog.Filter = "All files | *.*";
-            openFileDialog.InitialDirectory = @"C:\";
+            OpenFileDialog openFileDialog = new()
+            {
+                Title = "Select a file to ecnrypt",
+                Filter = "All files | *.*",
+                InitialDirectory = @"C:\"
+            };
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 // Isolate the filename from the filepath
                 string openFileName = FileNameIsolator.IsolateFileName(openFileDialog.FileName);
 
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Title = "Select a Destination for Encrypted File";
-                saveFileDialog.Filter = "Encrypted files |*.twwcts*";
+                SaveFileDialog saveFileDialog = new()
+                {
+                    Title = "Select a Destination for Encrypted File",
+                    Filter = "Encrypted files |*.twwcts*"
+                };
                 // If filename hash exists, use the associated GUID, else, create a new GUID (like below)
                 //saveFileDialog.FileName = GuidGenerator.GenerateNewGuid();
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
@@ -40,25 +44,17 @@ namespace SimpleSecUtility.Backend.EnDec
 
                     using (FileStream inputSream = new(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
                     {
-                        using (FileStream outputStream = new(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
-                        {
-                            using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
-                            {
-                                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-                                using (ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV))
-                                {
-                                    using (CryptoStream cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write))
-                                    {
-                                        byte[] buffer = new byte[1024];
-                                        int byteRead;
+                        using FileStream outputStream = new(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
+                        using System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create();
+                        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                        using ICryptoTransform encryptor = aes.CreateEncryptor(passwordBytes, _IV);
+                        using CryptoStream cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write);
+                        byte[] buffer = new byte[1024];
+                        int byteRead;
 
-                                        while ((byteRead = inputSream.Read(buffer, 0, buffer.Length)) > 0)
-                                        {
-                                            cryptoStream.Write(buffer, 0, byteRead);
-                                        }
-                                    }
-                                }
-                            }
+                        while ((byteRead = inputSream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            cryptoStream.Write(buffer, 0, byteRead);
                         }
                     }
                     File.Delete(openFileDialog.FileName);
@@ -68,15 +64,19 @@ namespace SimpleSecUtility.Backend.EnDec
 
         public static void ScanMetaAndDecryptFile(string password) // Explore the idea of passing through a Form object to open the password promtps, or not and continue with the script!
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Title = "Select a File to Decrypt";
-            openFileDialog.Filter = "Encrypted files |*.twwcts*";
-            openFileDialog.InitialDirectory = @"C:\";
+            OpenFileDialog openFileDialog = new()
+            {
+                Title = "Select a File to Decrypt",
+                Filter = "Encrypted files |*.twwcts*",
+                InitialDirectory = @"C:\"
+            };
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Title = "Select a Destination for Decrypted File";
-                saveFileDialog.Filter = "All files | *.*"; // Extension of og file path
+                SaveFileDialog saveFileDialog = new()
+                {
+                    Title = "Select a Destination for Decrypted File",
+                    Filter = "All files | *.*" // Extension of og file path
+                };
                 //saveFileDialog.FileName = ""; // og file path name
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
@@ -90,25 +90,17 @@ namespace SimpleSecUtility.Backend.EnDec
 
                     using (FileStream inputStream = new(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
                     {
-                        using (FileStream outputStream = new(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
-                        {
-                            using (System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create())
-                            {
-                                byte[] passwordBytes = Encoding.UTF8.GetBytes(password); // Credential.Password
-                                using (ICryptoTransform decryptor = aes.CreateDecryptor(passwordBytes, _IV))
-                                {
-                                    using (CryptoStream cryptoStream = new(outputStream, decryptor, CryptoStreamMode.Write))
-                                    {
-                                        byte[] buffer = new byte[1024];
-                                        int byteRead;
+                        using FileStream outputStream = new(saveFileDialog.FileName, FileMode.Create, FileAccess.Write);
+                        using System.Security.Cryptography.Aes aes = System.Security.Cryptography.Aes.Create();
+                        byte[] passwordBytes = Encoding.UTF8.GetBytes(password); // Credential.Password
+                        using ICryptoTransform decryptor = aes.CreateDecryptor(passwordBytes, _IV);
+                        using CryptoStream cryptoStream = new(outputStream, decryptor, CryptoStreamMode.Write);
+                        byte[] buffer = new byte[1024];
+                        int byteRead;
 
-                                        while ((byteRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
-                                        {
-                                            cryptoStream.Write(buffer, 0, byteRead);
-                                        }
-                                    }
-                                }
-                            }
+                        while ((byteRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            cryptoStream.Write(buffer, 0, byteRead);
                         }
                     }
                     File.Delete(openFileDialog.FileName);
