3a4212c34b7c96a730842f92b1c98cd7247fefe3
twwct <twwc.tech@gmail.com>
2023-11-24 21:26:34 -0700

    variables removed

--- AppResources/Statics.cs
+++ AppResources/Statics.cs
@@ -2,19 +2,7 @@
 {
     internal class Statics
     {
-        public const string AppVersion = "1.0";
-        public const string RegPath = @"Software\SimpleSecUtility";
-
         public const string PasswordRequirementsInfo = "Password Requirements:\n- Mininum of 8 characters or more in length\n- Minimum of 1 Number\n- Minimum of 1 Special Character [-!@#$%^&*()?_,.]";
-
-        public static string SetupInfo = "Welcome to the Simple Sec Utility Setup.\n" +
-            "The application is setup per user account, each new user\n" +
-            "or an existing user, using the app for the first time,\n" +
-            " will be asked to begin setup.\n\n" +
-            "To continue with the app, a 'Master Password' must be created\n" +
-            "to ensure only the active user account\n" +
-            $"({Environment.UserName}) has access to the 'File Encryption' tool.";
-
         public const string SpecialCharacters = "!@$%?";
     }
 }
