d6acddc4de7ae8836ff9491b5d6b300362c4eb94
twwct <twwc.tech@gmail.com>
2023-11-24 21:26:44 -0700

    logic updated

--- Backend/SecureGenerator/PassPin.cs
+++ Backend/SecureGenerator/PassPin.cs
@@ -1,12 +1,10 @@
-﻿using SimpleSecUtility.Backend.WinCredsManager;
-
-namespace SimpleSecUtility.Backend.SecureGenerator
+﻿namespace SimpleSecUtility.Backend.SecureGenerator
 {
     internal class PassPin
     {
         public static async Task<string> ReturnSecurePasswordOrPIN(string requestType, int requestLength)
         {
-            using HttpClient requestClient = new HttpClient();
+            using HttpClient requestClient = new();
             string apiUrl = string.Empty;
 
             apiUrl = requestType.ToLower().Trim() switch
@@ -15,29 +13,22 @@ namespace SimpleSecUtility.Backend.SecureGenerator
                 "pin" => $"https://www.random.org/strings/?num=1&len={requestLength}&digits=on&unique=on&format=plain&rnd=new",
                 _ => throw new ArgumentException("Not an option, please select from: [password, pin]"),
             };
-            string requestResult = string.Empty;
-            if (ManageWinCreds.DoesCredentialExist("SSU_api"))
-            {
-                try
-                {
-                    requestClient.DefaultRequestHeaders.Add("X-RandomOrg-ApiKey", ManageWinCreds.GetSecret("SSU_api"));
-                    HttpResponseMessage response = await requestClient.GetAsync(apiUrl);
 
-                    if (response.IsSuccessStatusCode)
-                    {
-                        requestResult = await response.Content.ReadAsStringAsync();
-                    }
-                    else
-                    {
-                        MessageBox.Show($"Unable to connect to RandomData.Org, Status Code: {response.StatusCode}", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                catch (Exception ex) { ex.ToString(); }
-            }
-            else
+            string requestResult = string.Empty;
+            try
             {
-                MessageBox.Show("API Key was not found", "API Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                HttpResponseMessage response = await requestClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    requestResult = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    MessageBox.Show($"Unable to connect to RandomData.Org, Status Code: {response.StatusCode}", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
+            catch (Exception ex) { ex.ToString(); }
             return requestResult.Trim();
         }
     }
