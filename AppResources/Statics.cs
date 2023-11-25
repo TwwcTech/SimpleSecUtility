502b7fc43d1e2557c639fd379b6147e783fbdf31
twwct <twwc.tech@gmail.com>
2023-11-24 21:45:25 -0700

    deleted

--- AppResources/Statics.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimpleSecUtility.AppResources
-{
-    internal class Statics
-    {
-        public const string PasswordRequirementsInfo = "Password Requirements:\n- Mininum of 8 characters or more in length\n- Minimum of 1 Number\n- Minimum of 1 Special Character [-!@#$%^&*()?_,.]";
-        public const string SpecialCharacters = "!@$%?";
-    }
-}
